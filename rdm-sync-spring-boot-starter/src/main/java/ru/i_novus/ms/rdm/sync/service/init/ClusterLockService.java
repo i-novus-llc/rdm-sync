ffac55d119f5803a30834f605ecb69fe47e73599
arahmatullin <arahmatullin@i-novus.ru>
2021-08-09 11:33:59 +0300

    bf RDM-918 Исправление используемого класса при создании логера.

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/init/ClusterLockService.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/init/ClusterLockService.java
@@ -8,33 +8,38 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import ru.i_novus.ms.rdm.sync.service.RdmSyncRestImpl;
 
 import java.sql.Timestamp;
 
 @Component
 class ClusterLockService {
 
-    private static final Logger logger = LoggerFactory.getLogger(RdmSyncRestImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(ClusterLockService.class);
 
-    @Autowired private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     public boolean tryLock() {
+
         boolean acquired;
+
         try {
             jdbcTemplate.queryForObject("SELECT last_acquired FROM rdm_sync.cluster_lock FOR UPDATE NOWAIT", Timestamp.class);
             acquired = true;
+
         } catch (CannotAcquireLockException ex) {
+
             logger.warn("Cannot acquire lock.", ex);
             acquired = false;
         }
+
         if (acquired) {
             jdbcTemplate.update("UPDATE rdm_sync.cluster_lock SET last_acquired = (SELECT CURRENT_TIMESTAMP AT TIME ZONE 'UTC')");
             logger.info("Lock successfully acquired.");
-            return true;
         }
-        return false;
+
+        return acquired;
     }
 
 }
