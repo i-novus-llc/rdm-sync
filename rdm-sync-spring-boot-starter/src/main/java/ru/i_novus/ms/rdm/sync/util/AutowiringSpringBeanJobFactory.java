85dadb9ada694e6084ee05c7257f12cdc0a67f92
arahmatullin <arahmatullin@i-novus.ru>
2021-08-11 12:34:08 +0300

    nf RDM-912 Поддержка Autowired для quartz-job'ов.

---
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/util/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,28 @@
+package ru.i_novus.ms.rdm.sync.util;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import javax.annotation.Nonnull;
+
+public class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory {
+
+    private final AutowireCapableBeanFactory beanFactory;
+
+    public AutowiringSpringBeanJobFactory(final ApplicationContext context) {
+
+        this.beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    @Nonnull
+    protected Object createJobInstance(@Nonnull TriggerFiredBundle bundle) throws Exception {
+
+        Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+
+        return job;
+    }
+}
