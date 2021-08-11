242bf6feef1dac9a6e7359a466b2438354591a5c
arahmatullin <arahmatullin@i-novus.ru>
2021-08-11 15:08:25 +0300

    cf RDM-912 Подключение настроек quartz и DataSource.

--- rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncAppConfig.java
+++ rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncAppConfig.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import ru.i_novus.ms.rdm.sync.quartz.AutowiringSpringBeanJobFactory;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SyncAppConfig {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public AutowiringSpringBeanJobFactory autowiringSpringBeanJobFactory() {
@@ -21,9 +27,11 @@ public class SyncAppConfig {
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
 
-        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-        schedulerFactoryBean.setJobFactory(autowiringSpringBeanJobFactory());
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setJobFactory(autowiringSpringBeanJobFactory());
+        schedulerFactory.setConfigLocation(new ClassPathResource("quartz.properties"));
+        schedulerFactory.setDataSource(dataSource);
 
-        return schedulerFactoryBean;
+        return schedulerFactory;
     }
 }
