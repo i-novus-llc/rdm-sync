54666ccdd8ef564b5de5a45a27c183b9da1674ce
arahmatullin <arahmatullin@i-novus.ru>
2021-08-11 12:45:01 +0300

    nf RDM-912 Scheduler с Autowired.

--- rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncAppConfig.java
+++ rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncAppConfig.java
@@ -1,7 +1,29 @@
 package ru.i_novus.ms.rdm.sync;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import ru.i_novus.ms.rdm.sync.util.AutowiringSpringBeanJobFactory;
 
 @Configuration
 public class SyncAppConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public AutowiringSpringBeanJobFactory autowiringSpringBeanJobFactory() {
+        return new AutowiringSpringBeanJobFactory(applicationContext);
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setJobFactory(autowiringSpringBeanJobFactory());
+
+        return schedulerFactoryBean;
+    }
 }
