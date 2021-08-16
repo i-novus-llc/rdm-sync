0f2bed5ec6b95838b92c4eee7af804d09ff6a563
arahmatullin <arahmatullin@i-novus.ru>
2021-08-16 10:38:15 +0300

    nf EGISZREMD-2142 Добавление класса - записи конфига по справочнику.

---
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/RdmSyncRow.java
@@ -0,0 +1,42 @@
+package ru.i_novus.ms.rdm.sync.admin.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Запись конфигурации по синхронизируемому справочнику.
+ */
+@Getter
+@Setter
+public class RdmSyncRow {
+
+    /** Код справочника. */
+    private String code;
+
+    /** Наименование справочника. */
+    private String name;
+
+    /** Версия (номер) справочника. */
+    private String version;
+
+    /** Признак версионности. */
+    private Boolean versioned;
+
+    /** Признак автообновления. */
+    private Boolean autoUpdatable;
+
+    /** Источник для обновления. */
+    private String source;
+
+    /** Дата последнего обновления. */
+    private LocalDateTime lastDateTime;
+
+    /** Статус последнего обновления. */
+    private Boolean lastStatus;
+
+    public RdmSyncRow() {
+        // Nothing to do.
+    }
+}
