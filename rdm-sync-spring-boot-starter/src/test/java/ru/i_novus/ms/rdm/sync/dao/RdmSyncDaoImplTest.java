830b5c42a1f35475c5605cb44aaec6095587aa5c
arahmatullin <arahmatullin@i-novus.ru>
2021-08-16 11:38:21 +0300

    cf RDM-912 Тест `RdmSyncDaoImpl` (mappings).

---
+++ rdm-sync-spring-boot-starter/src/test/java/ru/i_novus/ms/rdm/sync/dao/RdmSyncDaoImplTest.java
@@ -0,0 +1,159 @@
+package ru.i_novus.ms.rdm.sync.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import ru.i_novus.ms.rdm.sync.api.mapping.FieldMapping;
+import ru.i_novus.ms.rdm.sync.api.mapping.VersionMapping;
+import ru.i_novus.ms.rdm.sync.service.RdmMappingService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@SuppressWarnings({"rawtypes","unchecked"})
+public class RdmSyncDaoImplTest {
+
+    private static final String REFBOOK_CODE = "test";
+    private static final String REFBOOK_VERSION = "1.2";
+    private static final LocalDateTime PUBLICATION_DATE = LocalDateTime.of(2021, 1, 1, 1, 1);
+
+    private static final int LOCAL_ID = 10;
+    private static final String LOCAL_TABLE = "ltab";
+
+    @InjectMocks
+    private RdmSyncDaoImpl dao;
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @Mock
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Mock
+    private RdmMappingService rdmMappingService;
+
+    @Before
+    public void setUp() {
+
+        //when(namedParameterJdbcTemplate.getJdbcTemplate()).thenReturn(jdbcTemplate);
+    }
+
+    @Test
+    public void testGetVersionMappings() {
+
+        VersionMapping versionMapping = newVersionMapping();
+
+        List<VersionMapping> versionMappings = singletonList(versionMapping);
+        when(namedParameterJdbcTemplate.query(any(String.class), any(RowMapper.class)))
+                .thenReturn(versionMappings);
+
+        List<VersionMapping> list = dao.getVersionMappings();
+        assertSame(versionMappings, list);
+
+        ArgumentCaptor<String> sqlCaptor = ArgumentCaptor.forClass(String.class);
+        verify(namedParameterJdbcTemplate).query(sqlCaptor.capture(), any(RowMapper.class));
+
+        String sql = sqlCaptor.getValue();
+        assertNotNull(sql);
+        assertFalse(sql.contains(":code"));
+
+        verifyNoMore();
+    }
+
+    @Test
+    public void testGetVersionMapping() {
+
+        VersionMapping versionMapping = newVersionMapping();
+        when(namedParameterJdbcTemplate.query(any(String.class), any(Map.class), any(RowMapper.class)))
+                .thenReturn(singletonList(versionMapping));
+
+        VersionMapping item = dao.getVersionMapping(REFBOOK_CODE);
+        assertSame(versionMapping, item);
+
+        ArgumentCaptor<String> sqlCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Map> mapCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(namedParameterJdbcTemplate).query(sqlCaptor.capture(), mapCaptor.capture(), any(RowMapper.class));
+
+        String sql = sqlCaptor.getValue();
+        assertNotNull(sql);
+        assertTrue(sql.contains(":code"));
+
+        Map<String, Object> map = (Map<String, Object>) mapCaptor.getValue();
+        assertNotNull(map);
+        assertEquals(REFBOOK_CODE, map.get("code"));
+
+        verifyNoMore();
+    }
+
+    @Test
+    public void testGetLastVersion() {
+
+        when(namedParameterJdbcTemplate.query(any(String.class), any(Map.class), any(RowMapper.class)))
+                .thenReturn(singletonList(LOCAL_ID));
+
+        int result = dao.getLastVersion(REFBOOK_CODE);
+        assertEquals(LOCAL_ID, result);
+
+        verify(namedParameterJdbcTemplate).query(any(String.class), any(Map.class), any(RowMapper.class));
+
+        verifyNoMore();
+    }
+
+    @Test
+    public void testGetFieldMapping() {
+
+        List<FieldMapping> fieldMappings = newFieldMappings();
+        when(namedParameterJdbcTemplate.query(any(String.class), any(Map.class), any(RowMapper.class)))
+                .thenReturn(fieldMappings);
+
+        List<FieldMapping> list = dao.getFieldMapping(REFBOOK_CODE);
+        assertSame(fieldMappings, list);
+
+        ArgumentCaptor<String> sqlCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Map> mapCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(namedParameterJdbcTemplate).query(sqlCaptor.capture(), mapCaptor.capture(), any(RowMapper.class));
+
+        String sql = sqlCaptor.getValue();
+        assertNotNull(sql);
+        assertTrue(sql.contains(":code"));
+
+        Map<String, Object> map = (Map<String, Object>) mapCaptor.getValue();
+        assertNotNull(map);
+        assertEquals(REFBOOK_CODE, map.get("code"));
+
+        verifyNoMore();
+    }
+
+    private VersionMapping newVersionMapping() {
+
+        return new VersionMapping(LOCAL_ID, REFBOOK_CODE, REFBOOK_VERSION, PUBLICATION_DATE,
+                LOCAL_TABLE, "primary", "deleted", LocalDateTime.now(), LocalDateTime.now());
+    }
+
+    private List<FieldMapping> newFieldMappings() {
+
+        FieldMapping idMapping = new FieldMapping("code", "integer", "id");
+        FieldMapping nameMapping = new FieldMapping("text", "varchar", "name");
+
+        return List.of(idMapping, nameMapping);
+    }
+
+    private void verifyNoMore() {
+
+        verifyNoMoreInteractions(jdbcTemplate, namedParameterJdbcTemplate, rdmMappingService);
+    }
+}
