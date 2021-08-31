0ae37fdb23c1535bccebf07b954469f8e73637e0
arahmatullin <arahmatullin@i-novus.ru>
2021-08-31 14:11:20 +0300

    cf EGISZREMD-2156 Модель создания записи о синхронизации.

---
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/row/RdmSyncRowCreateRequest.java
@@ -0,0 +1,33 @@
+package ru.i_novus.ms.rdm.sync.admin.api.model.row;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@ApiModel(value = "Модель создания записи о синхронизации",
+        description = "Набор входных параметров для создания записи о синхронизации")
+@Getter
+@Setter
+public class RdmSyncRowCreateRequest implements Serializable {
+
+    @ApiModelProperty("Источник для обновления")
+    private String sourceType;
+
+    @ApiModelProperty("Код справочника")
+    private String code;
+
+    @ApiModelProperty("Наименование справочника")
+    private String name;
+
+    @ApiModelProperty("Версия (номер) справочника, с которой планируется загрузка справочника")
+    private String startVersion;
+
+    @ApiModelProperty("Поддержка версионности")
+    private Boolean versioned;
+
+    @ApiModelProperty("Признак автообновления")
+    private Boolean autoUpdatable;
+}
