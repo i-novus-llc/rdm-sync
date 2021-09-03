3d61d0d5a2b0983909a1a44d97ed8d073c260fe8
arahmatullin <arahmatullin@i-novus.ru>
2021-09-03 09:46:20 +0300

    nf EGISZREMD-2156 Классы для маппинга справочников.

---
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/mapping/FieldToFieldMapping.java
@@ -0,0 +1,21 @@
+package ru.i_novus.ms.rdm.sync.admin.api.model.mapping;
+
+public class FieldToFieldMapping extends SyncToFieldMapping {
+
+    private String oldField;
+
+    public FieldToFieldMapping(String newField, String oldField) {
+
+        super(newField);
+        
+        this.oldField = oldField;
+    }
+
+    public String getOldField() {
+        return oldField;
+    }
+
+    public void setOldField(String oldField) {
+        this.oldField = oldField;
+    }
+}
