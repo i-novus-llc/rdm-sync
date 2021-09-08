27ecd3ef8cf8269224fb95182e0baf8159f11f30
arahmatullin <arahmatullin@i-novus.ru>
2021-09-08 12:38:07 +0300

    nf EGISZREMD-2156 UI: Каркас создания маппинга.

---
+++ rdm-sync-admin-n2o/src/main/java/ru/i_novus/ms/rdm/sync/admin/n2o/provider/SyncMappingObjectProvider.java
@@ -0,0 +1,44 @@
+package ru.i_novus.ms.rdm.sync.admin.n2o.provider;
+
+import net.n2oapp.framework.api.metadata.SourceMetadata;
+import net.n2oapp.framework.api.metadata.global.dao.object.N2oObject;
+import net.n2oapp.framework.api.register.DynamicMetadataProvider;
+import org.springframework.stereotype.Service;
+import ru.i_novus.ms.rdm.sync.admin.n2o.model.SyncMappingRequest;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+@Service
+public class SyncMappingObjectProvider extends SyncMappingBaseProvider implements DynamicMetadataProvider {
+
+    static final String OBJECT_PROVIDER_ID = "syncMappingObject";
+
+    @Override
+    public String getCode() {
+        return OBJECT_PROVIDER_ID;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<? extends SourceMetadata> read(String context) {
+
+        SyncMappingRequest request = toRequest(context);
+        return singletonList(createObject(request));
+    }
+
+    @Override
+    public Collection<Class<? extends SourceMetadata>> getMetadataClasses() {
+        return singletonList(N2oObject.class);
+    }
+
+    private N2oObject createObject(SyncMappingRequest request) {
+
+        N2oObject n2oObject = new N2oObject();
+        //n2oObject.setOperations(createOperations(request));
+
+        return n2oObject;
+    }
+}
---
+++ rdm-sync-admin-n2o/src/main/java/ru/i_novus/ms/rdm/sync/admin/n2o/provider/SyncMappingQueryProvider.java
@@ -0,0 +1,45 @@
+package ru.i_novus.ms.rdm.sync.admin.n2o.provider;
+
+import net.n2oapp.framework.api.metadata.SourceMetadata;
+import net.n2oapp.framework.api.metadata.global.dao.N2oQuery;
+import net.n2oapp.framework.api.register.DynamicMetadataProvider;
+import org.springframework.stereotype.Service;
+import ru.i_novus.ms.rdm.sync.admin.n2o.model.SyncMappingRequest;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+@Service
+public class SyncMappingQueryProvider extends SyncMappingBaseProvider implements DynamicMetadataProvider {
+
+    static final String QUERY_PROVIDER_ID = "syncMappingQuery";
+
+    @Override
+    public String getCode() {
+        return QUERY_PROVIDER_ID;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<? extends SourceMetadata> read(String context) {
+
+        SyncMappingRequest request = toRequest(context);
+        return singletonList(createQuery(request));
+    }
+
+    @Override
+    public Collection<Class<? extends SourceMetadata>> getMetadataClasses() {
+        return singletonList(N2oQuery.class);
+    }
+
+    private N2oQuery createQuery(SyncMappingRequest request) {
+
+        N2oQuery n2oQuery = new N2oQuery();
+        //n2oQuery.setUniques(new N2oQuery.Selection[]{ createSelection() });
+        //n2oQuery.setFields(createQueryFields(request));
+
+        return n2oQuery;
+    }
+}
