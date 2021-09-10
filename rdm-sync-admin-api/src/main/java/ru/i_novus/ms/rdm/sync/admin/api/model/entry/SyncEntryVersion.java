7abd19f6e9b9d90bfaf3b9b469a258d1289beac5
arahmatullin <arahmatullin@i-novus.ru>
2021-09-10 08:51:24 +0300

    cf EGISZREMD-2156 Версия записи о синхронизации.

---
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/entry/SyncEntryVersion.java
@@ -0,0 +1,79 @@
+package ru.i_novus.ms.rdm.sync.admin.api.model.entry;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.i_novus.ms.rdm.sync.admin.api.utils.JsonUtil;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Модель версии записи о синхронизации справочника.
+ */
+@Getter
+@Setter
+public class SyncEntryVersion implements Serializable {
+
+    /** Идентификатор версии записи. */
+    private String id;
+
+    /** Идентификатор записи. */
+    private String entryId;
+
+    /** Код (справочника в) версии. */
+    private String code;
+
+    /** Наименование (справочника в) версии. */
+    private String name;
+
+    /** Версия (номер) справочника. */
+    private String version;
+
+    /** Дата начала действия. */
+    private LocalDateTime fromDate;
+
+    /** Дата окончания действия. */
+    private LocalDateTime toDate;
+
+    /** Дата последнего обновления. */
+    private LocalDateTime lastDateTime;
+
+    /** Статус последнего обновления. */
+    private String lastStatus;
+
+    public SyncEntryVersion() {
+        // Nothing to do.
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SyncEntryVersion that = (SyncEntryVersion) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(entryId, that.entryId) &&
+                Objects.equals(code, that.code) &&
+                Objects.equals(name, that.name) &&
+
+                Objects.equals(version, that.version) &&
+                Objects.equals(fromDate, that.fromDate) &&
+                Objects.equals(toDate, that.toDate) &&
+
+                Objects.equals(lastDateTime, that.lastDateTime) &&
+                Objects.equals(lastStatus, that.lastStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, entryId, code, name,
+                version, fromDate, toDate,
+                lastDateTime, lastStatus);
+    }
+
+    @Override
+    public String toString() {
+        return JsonUtil.toJsonString(this);
+    }
+}
