c4c56b73ce790e0074530edff88df3d3d36dd8f5
arahmatullin <arahmatullin@i-novus.ru>
2021-09-10 13:41:45 +0300

    cf EGISZREMD-2156 Рефакторинг после изменения построения маппинга.

--- rdm-sync-admin-n2o/src/main/java/ru/i_novus/ms/rdm/sync/admin/n2o/SyncN2oConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.i_novus.ms.rdm.sync.admin.n2o;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@SuppressWarnings({"rawtypes","java:S3740"})
-public class SyncN2oConfig {
-}
