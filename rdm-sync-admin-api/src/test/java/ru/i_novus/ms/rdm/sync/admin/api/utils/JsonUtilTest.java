620eb4d41cbdf3312adbd685bcd6361707c0c19b
arahmatullin <arahmatullin@i-novus.ru>
2021-09-16 15:09:53 +0300

    cf EGISZREMD-2156 Тест ошибочной логики JsonUtil.

--- rdm-sync-admin-api/src/test/java/ru/i_novus/ms/rdm/sync/admin/api/utils/JsonUtilTest.java
+++ rdm-sync-admin-api/src/test/java/ru/i_novus/ms/rdm/sync/admin/api/utils/JsonUtilTest.java
@@ -1,10 +1,11 @@
 package ru.i_novus.ms.rdm.sync.admin.api.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import ru.i_novus.ms.rdm.sync.admin.api.model.AbstractCriteria;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class JsonUtilTest {
 
@@ -20,5 +21,51 @@ public class JsonUtilTest {
         assertNotNull(newCriteria);
 
         assertEquals(oldCriteria, newCriteria);
+    }
+
+    @Test
+    public void testJsonStringWhenError() {
+
+        ObjectMapper testMapper = new TestObjectMapper();
+
+        try {
+            JsonUtil.toJsonString(testMapper, new AbstractCriteria());
+            fail();
+
+        } catch (RuntimeException e) {
+
+            assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+
+        try {
+            JsonUtil.fromJsonString(testMapper, "{}", AbstractCriteria.class);
+            fail();
+
+        } catch (RuntimeException e) {
+
+            assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+    }
+
+    private static class TestObjectMapper extends ObjectMapper {
+
+        @Override
+        public <T> T readValue(String content, Class<T> valueType) throws TestJsonProcessingException {
+
+            throw new TestJsonProcessingException("Test exception for readValue");
+        }
+
+        @Override
+        public String writeValueAsString(Object value) throws TestJsonProcessingException {
+
+            throw new TestJsonProcessingException("Test exception for writeValueAsString");
+        }
+    }
+
+    private static class TestJsonProcessingException extends JsonProcessingException {
+
+        public TestJsonProcessingException(String msg) {
+            super(msg);
+        }
     }
 }
