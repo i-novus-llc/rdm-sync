c1349448ddc08d7e807494197a6080c0afc334e2
Timur Nurdinov <tnurdinov@i-novus.ru>
2021-11-24 16:33:33 +0300

    bf RDM-919 не используемый импорт

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/RdmSyncServiceImpl.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/RdmSyncServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.i_novus.ms.rdm.sync.service;
 
-import org.apache.cxf.jaxrs.utils.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
