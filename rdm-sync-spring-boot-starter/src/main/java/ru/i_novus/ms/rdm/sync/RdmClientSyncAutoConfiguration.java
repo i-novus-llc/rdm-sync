af080a8c797936423a66d7dcd7b06d7cb5853123
arahmatullin <arahmatullin@i-novus.ru>
2021-11-24 09:32:38 +0300

    cf RDM-934 Перенос конфига для job и quartz.

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/RdmClientSyncAutoConfiguration.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/RdmClientSyncAutoConfiguration.java
@@ -4,6 +4,7 @@ import liquibase.integration.spring.SpringLiquibase;
 import net.n2oapp.platform.jaxrs.LocalDateTimeISOParameterConverter;
 import net.n2oapp.platform.jaxrs.TypedParamConverter;
 import net.n2oapp.platform.jaxrs.autoconfigure.MissingGenericBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -13,12 +14,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClas
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import ru.i_novus.ms.rdm.api.model.version.AttributeFilter;
@@ -30,6 +34,7 @@ import ru.i_novus.ms.rdm.sync.api.service.LocalRdmDataService;
 import ru.i_novus.ms.rdm.sync.api.service.RdmSyncService;
 import ru.i_novus.ms.rdm.sync.dao.RdmSyncDao;
 import ru.i_novus.ms.rdm.sync.dao.RdmSyncDaoImpl;
+import ru.i_novus.ms.rdm.sync.quartz.AutowiringSpringBeanJobFactory;
 import ru.i_novus.ms.rdm.sync.service.*;
 import ru.i_novus.ms.rdm.sync.service.change_data.*;
 import ru.i_novus.ms.rdm.sync.service.init.LocalRefBookCreator;
@@ -59,6 +64,28 @@ import java.util.Map;
         value = "rdm_sync.enabled",
         matchIfMissing = true)
 public class RdmClientSyncAutoConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public AutowiringSpringBeanJobFactory autowiringSpringBeanJobFactory() {
+        return new AutowiringSpringBeanJobFactory(applicationContext);
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setJobFactory(autowiringSpringBeanJobFactory());
+        schedulerFactory.setConfigLocation(new ClassPathResource("quartz.properties"));
+        schedulerFactory.setDataSource(dataSource);
+
+        return schedulerFactory;
+    }
 
     @Bean
     @ConditionalOnMissingBean
