fc2dcafc00d2c4d80562ca462ae3dbf5428ef01f
arahmatullin <arahmatullin@i-novus.ru>
2021-11-25 08:04:15 +0300

    bf RDM-934 Исправление создания quartz-таблиц в тесте.

--- rdm-sync-spring-boot-starter/src/test/java/ru/i_novus/ms/rdm/sync/dao/BaseDaoTest.java
+++ rdm-sync-spring-boot-starter/src/test/java/ru/i_novus/ms/rdm/sync/dao/BaseDaoTest.java
@@ -6,7 +6,14 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@JdbcTest(properties = {"spring.liquibase.change-log=classpath:/rdm-sync-db/baseChangelog.xml"})
-@AutoConfigureEmbeddedDatabase(type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES, provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.OPENTABLE)
+@JdbcTest(properties = {
+        "spring.liquibase.change-log=classpath:/rdm-sync-db/baseChangelog.xml",
+        "spring.liquibase.parameters.quartz_schema_name=rdm_sync_qz",
+        "spring.liquibase.parameters.quartz_table_prefix=rdm_sync_qrtz_"
+})
+@AutoConfigureEmbeddedDatabase(
+        type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES,
+        provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.OPENTABLE
+)
 public abstract class BaseDaoTest {
 }
