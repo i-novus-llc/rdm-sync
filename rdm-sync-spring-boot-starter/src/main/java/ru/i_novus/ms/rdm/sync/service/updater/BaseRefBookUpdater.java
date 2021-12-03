c06842b2fe81e84590567805710e21c5f0478164
shgaripov <shgaripov@i-novus.ru>
2021-12-03 15:13:16 +0300

    cf RDM-942 рефакторинг

---
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/updater/BaseRefBookUpdater.java
@@ -0,0 +1,122 @@
+package ru.i_novus.ms.rdm.sync.service.updater;
+
+import org.apache.cxf.jaxrs.utils.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.i_novus.ms.rdm.sync.api.mapping.FieldMapping;
+import ru.i_novus.ms.rdm.sync.api.mapping.LoadedVersion;
+import ru.i_novus.ms.rdm.sync.api.mapping.VersionMapping;
+import ru.i_novus.ms.rdm.sync.api.model.RefBook;
+import ru.i_novus.ms.rdm.sync.api.service.SyncSourceService;
+import ru.i_novus.ms.rdm.sync.dao.RdmSyncDao;
+import ru.i_novus.ms.rdm.sync.service.RdmLoggingService;
+import ru.i_novus.ms.rdm.sync.service.persister.PersisterServiceLocator;
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+public abstract class BaseRefBookUpdater implements RefBookUpdater{
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseRefBookUpdater.class);
+
+    private final RdmSyncDao dao;
+
+    private final SyncSourceService syncSourceService;
+
+    private final PersisterServiceLocator persisterServiceLocator;
+
+    private final RdmLoggingService loggingService;
+
+    public BaseRefBookUpdater(RdmSyncDao dao, SyncSourceService syncSourceService, PersisterServiceLocator persisterServiceLocator, RdmLoggingService loggingService) {
+        this.dao = dao;
+        this.syncSourceService = syncSourceService;
+        this.persisterServiceLocator = persisterServiceLocator;
+        this.loggingService = loggingService;
+    }
+
+    @Override
+    public void update(String refCode) {
+        if (dao.getVersionMapping(refCode, "CURRENT") == null) {
+            logger.error("No version mapping found for reference book with code '{}'.", refCode);
+            return;
+        }
+
+        RefBook newVersion;
+        try {
+            newVersion = getLastPublishedVersion(refCode);
+
+        } catch (Exception e) {
+            logger.error(String.format("Error while fetching new version with code '%s'.", refCode), e);
+            return;
+        }
+
+        VersionMapping versionMapping = getVersionMapping(refCode);
+        LoadedVersion loadedVersion = dao.getLoadedVersion(refCode);
+        try {
+            if (loadedVersion == null || isNewVersionPublished(newVersion, loadedVersion) || isMappingChanged(versionMapping, loadedVersion)) {
+
+                update(newVersion, versionMapping);
+                loggingService.logOk(refCode, versionMapping.getVersion(), newVersion.getLastVersion());
+
+            } else {
+                logger.info("Skipping update on '{}'. No changes.", refCode);
+            }
+        } catch (Exception e) {
+            logger.error(String.format("Error while updating new version with code '%s'.", refCode), e);
+
+            loggingService.logError(refCode, versionMapping.getVersion(), newVersion.getLastVersion(),
+                    e.getMessage(), ExceptionUtils.getStackTrace(e));
+        }
+
+    }
+
+    protected RefBook getLastPublishedVersion(String refBookCode) {
+        RefBook refBook = syncSourceService.getRefBook(refBookCode);
+        if (refBook == null)
+            throw new IllegalArgumentException(String.format("Reference book with code '%s' not found.", refBookCode));
+
+        if (!refBook.getStructure().hasPrimary())
+            throw new IllegalStateException(String.format("Reference book with code '%s' has not primary key.", refBookCode));
+        return refBook;
+    }
+
+    protected VersionMapping getVersionMapping(String refBookCode) {
+
+        VersionMapping versionMapping = dao.getVersionMapping(refBookCode, "CURRENT");
+        List<FieldMapping> fieldMappings = dao.getFieldMappings(versionMapping.getCode());
+
+        final String primaryField = versionMapping.getPrimaryField();
+        if (fieldMappings.stream().noneMatch(mapping -> mapping.getSysField().equals(primaryField)))
+            throw new IllegalArgumentException(String.format("No mapping found for primary key '%s'.", primaryField));
+
+        return versionMapping;
+    }
+
+    protected boolean isNewVersionPublished(RefBook newVersion, LoadedVersion loadedVersion) {
+
+        return !loadedVersion.getVersion().equals(newVersion.getLastVersion())
+                && !loadedVersion.getPublicationDate().equals(newVersion.getLastPublishDate());
+    }
+
+    protected boolean isMappingChanged(VersionMapping versionMapping, LoadedVersion loadedVersion) {
+        return versionMapping.getMappingLastUpdated().isAfter(loadedVersion.getLastSync());
+    }
+
+    protected void update(RefBook newVersion, VersionMapping versionMapping) {
+    }
+
+    protected void validateStructureAndMapping(RefBook newVersion, List<FieldMapping> fieldMappings) {
+
+        List<String> clientRdmFields = fieldMappings.stream().map(FieldMapping::getRdmField).collect(toList());
+        Set<String> actualFields = newVersion.getStructure().getAttributesAndTypes().keySet();
+        if (!actualFields.containsAll(clientRdmFields)) {
+            // В новой версии удалены поля, которые ведутся в системе
+            clientRdmFields.removeAll(actualFields);
+            throw new IllegalStateException(String.format("Field '%s' was deleted in version with code '%s'. Update your mappings.",
+                    String.join(",", clientRdmFields), newVersion.getCode()));
+        }
+
+    }
+}
--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/updater/NotVersionedRefBookUpdater.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/updater/NotVersionedRefBookUpdater.java
@@ -1,6 +1,5 @@
 package ru.i_novus.ms.rdm.sync.service.updater;
 
-import org.apache.cxf.jaxrs.utils.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,14 +14,11 @@ import ru.i_novus.ms.rdm.sync.service.persister.PersisterService;
 import ru.i_novus.ms.rdm.sync.service.persister.PersisterServiceLocator;
 
 import java.util.List;
-import java.util.Set;
-
-import static java.util.stream.Collectors.toList;
 
 @Component
-public class NotVersionedRefBookUpdater implements RefBookUpdater {
+public class NotVersionedRefBookUpdater extends BaseRefBookUpdater {
 
-    private static final Logger logger = LoggerFactory.getLogger(NotVersionedRefBookUpdater.class);
+    private static Logger logger = LoggerFactory.getLogger(NotVersionedRefBookUpdater.class);
 
     private final RdmSyncDao dao;
 
@@ -30,85 +26,15 @@ public class NotVersionedRefBookUpdater implements RefBookUpdater {
 
     private final PersisterServiceLocator persisterServiceLocator;
 
-    private final RdmLoggingService loggingService;
-
     public NotVersionedRefBookUpdater(RdmSyncDao dao, SyncSourceService syncSourceService, PersisterServiceLocator persisterServiceLocator, RdmLoggingService loggingService) {
+        super(dao,syncSourceService,persisterServiceLocator, loggingService);
         this.dao = dao;
         this.syncSourceService = syncSourceService;
         this.persisterServiceLocator = persisterServiceLocator;
-        this.loggingService = loggingService;
     }
 
-    @Override
-    public void update(String refCode) {
-        if (dao.getVersionMapping(refCode, "CURRENT") == null) {
-            logger.error("No version mapping found for reference book with code '{}'.", refCode);
-            return;
-        }
 
-        RefBook newVersion;
-        try {
-            newVersion = getLastPublishedVersion(refCode);
-
-        } catch (Exception e) {
-            logger.error(String.format("Error while fetching new version with code '%s'.", refCode), e);
-            return;
-        }
-
-        VersionMapping versionMapping = getVersionMapping(refCode);
-        LoadedVersion loadedVersion = dao.getLoadedVersion(refCode);
-        try {
-            if (loadedVersion == null || isNewVersionPublished(newVersion, loadedVersion) || isMappingChanged(versionMapping, loadedVersion)) {
-
-                update(newVersion, versionMapping);
-                loggingService.logOk(refCode, versionMapping.getVersion(), newVersion.getLastVersion());
-
-            } else {
-                logger.info("Skipping update on '{}'. No changes.", refCode);
-            }
-        } catch (Exception e) {
-            logger.error(String.format("Error while updating new version with code '%s'.", refCode), e);
-
-            loggingService.logError(refCode, versionMapping.getVersion(), newVersion.getLastVersion(),
-                    e.getMessage(), ExceptionUtils.getStackTrace(e));
-        }
-
-    }
-
-    private RefBook getLastPublishedVersion(String refBookCode) {
-        RefBook refBook = syncSourceService.getRefBook(refBookCode);
-        if (refBook == null)
-            throw new IllegalArgumentException(String.format("Reference book with code '%s' not found.", refBookCode));
-
-        if (!refBook.getStructure().hasPrimary())
-            throw new IllegalStateException(String.format("Reference book with code '%s' has not primary key.", refBookCode));
-        return refBook;
-    }
-
-    private VersionMapping getVersionMapping(String refBookCode) {
-
-        VersionMapping versionMapping = dao.getVersionMapping(refBookCode, "CURRENT");
-        List<FieldMapping> fieldMappings = dao.getFieldMappings(versionMapping.getCode());
-
-        final String primaryField = versionMapping.getPrimaryField();
-        if (fieldMappings.stream().noneMatch(mapping -> mapping.getSysField().equals(primaryField)))
-            throw new IllegalArgumentException(String.format("No mapping found for primary key '%s'.", primaryField));
-
-        return versionMapping;
-    }
-
-    private boolean isNewVersionPublished(RefBook newVersion, LoadedVersion loadedVersion) {
-
-        return !loadedVersion.getVersion().equals(newVersion.getLastVersion())
-                && !loadedVersion.getPublicationDate().equals(newVersion.getLastPublishDate());
-    }
-
-    private boolean isMappingChanged(VersionMapping versionMapping, LoadedVersion loadedVersion) {
-        return versionMapping.getMappingLastUpdated().isAfter(loadedVersion.getLastSync());
-    }
-
-    private void update(RefBook newVersion, VersionMapping versionMapping) {
-
+    protected void update(RefBook newVersion, VersionMapping versionMapping){
         logger.info("{} sync started", newVersion.getCode());
         // Если изменилась структура, проверяем актуальность полей в маппинге
         List<FieldMapping> fieldMappings = dao.getFieldMappings(versionMapping.getCode());
@@ -148,18 +74,4 @@ public class NotVersionedRefBookUpdater implements RefBookUpdater {
             }
         }
     }
-
-    private void validateStructureAndMapping(RefBook newVersion, List<FieldMapping> fieldMappings) {
-
-        List<String> clientRdmFields = fieldMappings.stream().map(FieldMapping::getRdmField).collect(toList());
-        Set<String> actualFields = newVersion.getStructure().getAttributesAndTypes().keySet();
-        if (!actualFields.containsAll(clientRdmFields)) {
-            // В новой версии удалены поля, которые ведутся в системе
-            clientRdmFields.removeAll(actualFields);
-            throw new IllegalStateException(String.format("Field '%s' was deleted in version with code '%s'. Update your mappings.",
-                    String.join(",", clientRdmFields), newVersion.getCode()));
-        }
-
-    }
-
 }
