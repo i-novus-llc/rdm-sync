461d0346017f7f6804f89f3bd4846a45a19e2f3a
shgaripov <shgaripov@i-novus.ru>
2021-12-29 11:11:36 +0300

    cf story/RDM-962 add @DisallowConcurrentExecution

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/quartz/RdmSyncInitJob.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/quartz/RdmSyncInitJob.java
@@ -1,5 +1,6 @@
 package ru.i_novus.ms.rdm.sync.quartz;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import ru.i_novus.ms.rdm.sync.service.init.RdmSyncInitializer;
 
 @Component
+@DisallowConcurrentExecution
 public final class RdmSyncInitJob implements Job {
 
     public static final String NAME = "RdmSyncInitialize";
