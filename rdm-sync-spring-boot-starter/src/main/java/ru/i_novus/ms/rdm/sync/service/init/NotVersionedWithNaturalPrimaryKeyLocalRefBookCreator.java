173fcd8db2d96ba6c1790b81382002cf7e174f18
shgaripov <shgaripov@i-novus.ru>
2022-01-17 09:58:12 +0300

    cf RDM-965 creator, для создания справочников с первичным ключом из таблицы

---
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/init/NotVersionedWithNaturalPrimaryKeyLocalRefBookCreator.java
@@ -0,0 +1,23 @@
+package ru.i_novus.ms.rdm.sync.service.init;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.i_novus.ms.rdm.sync.api.dao.SyncSourceDao;
+import ru.i_novus.ms.rdm.sync.api.model.SyncTypeEnum;
+import ru.i_novus.ms.rdm.sync.api.service.SyncSourceServiceFactory;
+import ru.i_novus.ms.rdm.sync.dao.RdmSyncDao;
+
+import java.util.Set;
+
+@Component
+public class NotVersionedWithNaturalPrimaryKeyLocalRefBookCreator extends NotVersionedLocalRefBookCreator{
+    public NotVersionedWithNaturalPrimaryKeyLocalRefBookCreator(String schema, Boolean caseIgnore, RdmSyncDao dao, SyncSourceDao syncSourceDao, Set<SyncSourceServiceFactory> syncSourceServiceFactories) {
+        super(schema, caseIgnore, dao, syncSourceDao, syncSourceServiceFactories);
+    }
+
+    @Transactional
+    @Override
+    public void create(String refBookCode, String refBookName, String source, SyncTypeEnum type, String table) {
+        super.create(refBookCode, refBookName, source, type, table);
+    }
+}
