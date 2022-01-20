d2db2d427f0ff0d01cd14b790134d5d6bd55c9a3
shgaripov <shgaripov@i-novus.ru>
2022-01-20 09:58:54 +0300

    cf RDM-965 clean code

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/updater/NotVersionedRefBookUpdater.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/updater/NotVersionedRefBookUpdater.java
@@ -1,11 +1,6 @@
 package ru.i_novus.ms.rdm.sync.service.updater;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import ru.i_novus.ms.rdm.sync.api.mapping.LoadedVersion;
-import ru.i_novus.ms.rdm.sync.api.model.RefBookVersion;
 import ru.i_novus.ms.rdm.sync.api.service.SyncSourceService;
 import ru.i_novus.ms.rdm.sync.dao.RdmSyncDao;
 import ru.i_novus.ms.rdm.sync.service.RdmLoggingService;
