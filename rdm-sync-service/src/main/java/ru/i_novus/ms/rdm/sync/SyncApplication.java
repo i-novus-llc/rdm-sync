9c422f441b4e719774e511ceb20a1fb98397205d
shgaripov <shgaripov@i-novus.ru>
2022-02-22 12:29:52 +0300

    cf RDM-977 clean code

--- rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncApplication.java
+++ rdm-sync-service/src/main/java/ru/i_novus/ms/rdm/sync/SyncApplication.java
@@ -3,7 +3,6 @@ package ru.i_novus.ms.rdm.sync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class SyncApplication extends SpringBootServletInitializer {
