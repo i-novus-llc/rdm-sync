e79297026b31678ebd7206620fb2f843c6a1dad8
shgaripov <shgaripov@i-novus.ru>
2022-02-22 13:04:09 +0300

    cf RDM-977 clean code

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/RdmSyncServiceImpl.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/RdmSyncServiceImpl.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.i_novus.ms.rdm.api.exception.RdmException;
 import ru.i_novus.ms.rdm.sync.api.log.Log;
