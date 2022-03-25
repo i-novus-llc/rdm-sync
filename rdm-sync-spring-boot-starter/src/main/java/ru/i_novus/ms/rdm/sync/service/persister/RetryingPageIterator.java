4c8e87eaf5220d7ccd85ab1301fc4c7b5377989d
shgaripov <shgaripov@i-novus.ru>
2022-03-25 14:13:44 +0300

    nf RDM-992 рефакторинг

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/persister/RetryingPageIterator.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/persister/RetryingPageIterator.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 
 import java.util.Iterator;
+import java.util.function.Supplier;
 
 public class RetryingPageIterator<T, C extends RestCriteria> implements Iterator<Page<? extends T>> {
 
@@ -25,31 +26,27 @@ public class RetryingPageIterator<T, C extends RestCriteria> implements Iterator
     @SneakyThrows
     @Override
     public boolean hasNext() {
-        int count = 0;
-        while (count++ < tries) {
-            try {
-                return original.hasNext();
-            } catch (Throwable e) {
-                logger.warn(String.format("An error occurred, we will try again in %s seconds (%s tries left)", timeout / 1000, tries - count));
-                Thread.sleep(timeout);
-            }
-        }
-        return original.hasNext();
+        return retry(original::hasNext);
     }
 
     @SneakyThrows
     @Override
     public Page<? extends T> next() {
+        return retry(original::next);
+    }
+
+    private <E> E retry(Supplier<? extends E> supplier) throws InterruptedException {
         int count = 0;
         while (count++ < tries) {
             try {
-                return original.next();
+                return supplier.get();
             } catch (Throwable e) {
                 logger.warn(String.format("An error occurred, we will try again in %s seconds (%s tries left)", timeout / 1000, tries - count));
                 Thread.sleep(timeout);
             }
         }
-        return original.next();
+        return supplier.get();
     }
+
 
 }
