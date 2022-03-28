a867dcd813875023742123b972721b5921e1daa0
shgaripov <shgaripov@i-novus.ru>
2022-03-28 11:14:22 +0300

    cf RDM-992 clean code

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/persister/RetryingPageIterator.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/service/persister/RetryingPageIterator.java
@@ -1,7 +1,6 @@
 package ru.i_novus.ms.rdm.sync.service.persister;
 
 import lombok.SneakyThrows;
-import net.n2oapp.platform.jaxrs.RestCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -9,7 +8,7 @@ import org.springframework.data.domain.Page;
 import java.util.Iterator;
 import java.util.function.Supplier;
 
-public class RetryingPageIterator<T, C extends RestCriteria> implements Iterator<Page<? extends T>> {
+public class RetryingPageIterator<T> implements Iterator<Page<? extends T>> {
 
     private static Logger logger = LoggerFactory.getLogger(RetryingPageIterator.class);
 
