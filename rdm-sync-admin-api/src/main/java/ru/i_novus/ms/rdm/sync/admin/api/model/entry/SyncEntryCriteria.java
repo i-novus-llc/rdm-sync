da229d20b1f893954945da1cde6a5ed77c5b25a8
arahmatullin <arahmatullin@i-novus.ru>
2022-05-12 11:27:32 +0300

    nf EGISZREMD-3109 Возможность поиска записей по идентификатору источника.

--- rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/entry/SyncEntryCriteria.java
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/entry/SyncEntryCriteria.java
@@ -27,6 +27,10 @@ public class SyncEntryCriteria extends AbstractCriteria {
     @QueryParam("name")
     private String name;
 
+    @ApiParam("Идентификатор источника")
+    @QueryParam("sourceId")
+    private String sourceId;
+
     @ApiParam("Текст для поиска по нескольким полям")
     @QueryParam("text")
     private String text;
@@ -45,6 +49,7 @@ public class SyncEntryCriteria extends AbstractCriteria {
         return Objects.equals(id, that.id) &&
                 Objects.equals(code, that.code) &&
                 Objects.equals(name, that.name) &&
+                Objects.equals(sourceId, that.sourceId) &&
 
                 Objects.equals(text, that.text) &&
                 Objects.equals(count, that.count);
@@ -52,6 +57,6 @@ public class SyncEntryCriteria extends AbstractCriteria {
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, code, name, text, count);
+        return Objects.hash(super.hashCode(), id, code, name, sourceId, text, count);
     }
 }
