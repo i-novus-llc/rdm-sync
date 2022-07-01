c5f7e53f5e0bdc265ffcc913cb1a04213af8e96f
arahmatullin <arahmatullin@i-novus.ru>
2022-07-01 14:42:57 +0300

    bf EGISZREMD-3486 Поддержка сериализации SyncEntryMappingHint.

--- rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/entry/SyncEntryMappingHint.java
+++ rdm-sync-admin-api/src/main/java/ru/i_novus/ms/rdm/sync/admin/api/model/entry/SyncEntryMappingHint.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.i_novus.ms.rdm.sync.admin.api.utils.JsonUtil;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Подсказка по маппингу для версии записи.
@@ -12,9 +16,30 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SyncEntryMappingHint {
+public class SyncEntryMappingHint implements Serializable {
 
     private String id;
     private String name;
     private String href;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SyncEntryMappingHint that = (SyncEntryMappingHint) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(href, that.href);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, href);
+    }
+
+    @Override
+    public String toString() {
+        return JsonUtil.toJsonString(this);
+    }
 }
