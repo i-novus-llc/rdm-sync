6bd23264ade225b603cb02849f21dfafd942e27f
shgaripov <shgaripov@i-novus.ru>
2024-08-14 13:10:47 +0300

    fix tests
    https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1677

--- rdm-sync-spring-boot-starter/src/test/java/ru/i_novus/ms/rdm/sync/dao/RdmSyncDaoTest.java
+++ rdm-sync-spring-boot-starter/src/test/java/ru/i_novus/ms/rdm/sync/dao/RdmSyncDaoTest.java
@@ -25,6 +25,7 @@ import ru.i_novus.ms.rdm.sync.model.filter.FilterTypeEnum;
 import ru.i_novus.ms.rdm.sync.service.RdmMappingService;
 import ru.i_novus.ms.rdm.sync.service.RdmMappingServiceImpl;
 import ru.i_novus.ms.rdm.sync.service.VersionMappingServiceImpl;
+import ru.i_novus.ms.rdm.sync.util.MappingDataTransformer;
 
 import javax.ws.rs.BadRequestException;
 import java.sql.Date;
@@ -59,6 +60,9 @@ class RdmSyncDaoTest extends BaseDaoTest {
         public VersionMappingService versionMappingService() {
             return new VersionMappingServiceImpl(rdmSyncDao());
         }
+
+        @Bean
+        public MappingDataTransformer mappingDataTransformer() {return new MappingDataTransformer();}
     }
 
     @Autowired
