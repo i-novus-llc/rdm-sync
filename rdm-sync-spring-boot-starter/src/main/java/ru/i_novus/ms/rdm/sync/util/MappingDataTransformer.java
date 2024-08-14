02a55dbd8c957ca38ee647cedd20e9b73275b289
shgaripov <shgaripov@i-novus.ru>
2024-08-14 16:17:14 +0300

    Refactor MappingDataTransformer https://jira.i-novus.ru/browse/EGISZNRPLATFORM-1677

--- rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/util/MappingDataTransformer.java
+++ rdm-sync-spring-boot-starter/src/main/java/ru/i_novus/ms/rdm/sync/util/MappingDataTransformer.java
@@ -1,6 +1,5 @@
 package ru.i_novus.ms.rdm.sync.util;
 
-import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.SimpleEvaluationContext;
@@ -15,35 +14,10 @@ import java.util.Map;
 @Component
 public class MappingDataTransformer {
 
-    // Карта выражений, где ключ — это имя выражения, а значение — это строка SpEL выражения.
-    private static final Map<String, String> expressionMap = new HashMap<>();
-
     // Парсер для SpEL выражений
     private final ExpressionParser parser = new SpelExpressionParser();
 
-    static {
-        // Инициализация карты выражений. Пример: интерпретация boolean значения.
-        expressionMap.put("interpretBoolean",
-                "#root == null ? null : \n" +
-                "(#root == 1 || #root == 'истина') ? true : \n" +
-                "(#root == 0 || #root == 'ложь') ? false : \n" +
-                "false\n");
-    }
-
-    /**
-     * Оценивает выражение по ключу и применяет его к входному значению.
-     *
-     * @param key         Ключ выражения, соответствующий определенному SpEL выражению.
-     * @param input       Входное значение, которое будет использовано в выражении.
-     * @param returnType  Ожидаемый тип возвращаемого значения.
-     * @return Преобразованное значение на основе указанного выражения.
-     * @throws IllegalArgumentException Если выражение по указанному ключу не найдено.
-     */
-    public Object evaluateExpression(String key, Object input, Class<?> returnType) {
-        String expressionString = expressionMap.get(key);
-        if (expressionString == null) {
-            throw new IllegalArgumentException("Expression not found for key: " + key);
-        }
+    public Object evaluateExpression(String expr, Object input, Class<?> returnType) {
 
         // Создание контекста для оценки SpEL выражения
         SimpleEvaluationContext context = SimpleEvaluationContext
@@ -51,9 +25,7 @@ public class MappingDataTransformer {
                 .withRootObject(input)
                 .build();
 
-        // Оценка выражения и возвращение результата
-        Expression expression = parser.parseExpression(expressionString);
-        return expression.getValue(context, returnType);
+        return parser.parseExpression(expr).getValue(context, returnType);
     }
 
 }
